4033a69464d869d3050f87491ca51cc261f621a2
414634519 <139223400+414634519@users.noreply.github.com>
2024-11-30 12:15:11 +0800

    Update ticaibao.js

--- ticaibao.js
+++ ticaibao.js
@@ -1,7 +1,20 @@
-*/
-let obj = JSON.parse($response.body);
-obj.data.name = "Baby";
-obj.data.iEndTime = "1796515200";
-$done({
-    body: JSON.stringify(obj)
-});
+// Quantumult X Script to modify iEndTime to 2026-12-31 00:00:00
+const url = $request.url;
+const pathModule = "/json/crm_module/readModule";
+
+// 时间戳对应2026年12月31日00:00:00
+const newEndTime = 1798771200;
+
+if (url.includes(pathModule)) {
+    let obj = JSON.parse($response.body);
+
+    if (obj.rsp && obj.rsp.stModules) {
+        obj.rsp.stModules.forEach(module => {
+            module.iEndTime = newEndTime;
+        });
+    }
+
+    $done({ body: JSON.stringify(obj) });
+} else {
+    $done({});
+}
